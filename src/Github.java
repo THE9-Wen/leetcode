d68434a150f8e67ef1c837abe49704c540a0f3b1
liuhaoran_glb <liuhaoran_glb@qq.com>
2021-01-12 23:22:12 +0800

    test

---
+++ src/Github.java
@@ -0,0 +1,10 @@
+/**
+ * @author Wenhao Tong
+ * @Description
+ * @create 2021-01-12 23:15
+ */
+public class Github {
+    public static void main(String[] args) {
+        System.out.println("Hi Github!");
+    }
+}
