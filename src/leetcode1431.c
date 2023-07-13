3cd51628e6886af85071ad95c40a186c49df4c5f
Wenhao Tong <854772137@qq.com>
2023-07-13 22:25:28 +0800

    leetcode1431

---
+++ src/leetcode1431.c
@@ -0,0 +1,33 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <memory.h>
+
+//
+// Created by Wenhao on 2023/7/13.
+//
+bool *kidsWithCandies(int *candies, int candiesSize, int extraCandies, int *returnSize) {
+    *returnSize = candiesSize;
+    bool *result = (bool *) malloc(sizeof(bool) * candiesSize);
+    int max = 0;
+    for (int i = 0; i < candiesSize; i++) {
+        if (candies[i] > max) {
+            max = candies[i];
+        }
+    }
+    for (int i = 0; i < candiesSize; i++) {
+        if (candies[i] + extraCandies < max) {
+            result[i] = false;
+        } else {
+            result[i] = true;
+        }
+    }
+    return result;
+}
+
+int main() {
+    int array[4];
+    memset(array, 0, 4);
+    return 0;
+}
+
+
