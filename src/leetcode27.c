de92a3319adf2bc14c480bd58733d91d5d2a4dc2
Wenhao Tong <854772137@qq.com>
2023-07-14 21:46:12 +0800

    leetcode27

---
+++ src/leetcode27.c
@@ -0,0 +1,20 @@
+//
+// Created by Wenhao on 2023/7/14.
+//
+
+int removeElement(int* nums, int numsSize, int val){
+    int fast = 0;
+    int slow = 0;
+    while (fast < numsSize) {
+        if (nums[fast] != val && fast < numsSize) {
+            nums[slow++] = nums[fast++];
+        } else {
+            fast++;
+        }
+    }
+    return slow;
+}
+
+int main() {
+    return 0;
+}
